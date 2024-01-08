55ff14444eeba345315f67da3da0477b853a5ff1
sgantzer12 <74564975+sgantzer12@users.noreply.github.com>
2024-01-08 11:49:31 +0100

    fixed gears

--- Physics II - Bullet3D - Handout 05/ModulePlayer.cpp
+++ Physics II - Bullet3D - Handout 05/ModulePlayer.cpp
@@ -17,7 +17,7 @@ ModulePlayer::~ModulePlayer()
 bool ModulePlayer::Start()
 {
 	LOG("Loading player");
-
+	gears = 0;
 	VehicleInfo car;
 
 	// Car properties ----------------------------------------
@@ -212,7 +212,7 @@ update_status ModulePlayer::Update(float dt)
 	vehicle->Render();
 
 	char title[80];
-	sprintf_s(title, "%.1f Km/h", vehicle->GetKmh());
+	sprintf_s(title, "%.1f Km/h Gear: %d Revs: %.1f", vehicle->GetKmh(), gears, revs);
 	App->window->SetTitle(title);
 
 	return UPDATE_CONTINUE;
