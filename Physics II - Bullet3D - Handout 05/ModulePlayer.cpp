31687926466769a6c13564c15fae4a618f504cf5
Onnda <dannoman2004@gmail.com>
2024-01-10 23:17:08 +0100

    Mapa casi acabado

--- Physics II - Bullet3D - Handout 05/ModulePlayer.cpp
+++ Physics II - Bullet3D - Handout 05/ModulePlayer.cpp
@@ -146,7 +146,7 @@ bool ModulePlayer::Start()
 	//car.wheels[7].steering = false;
 
 	vehicle = App->physics->AddVehicle(car);
-	vehicle->SetPos(-293, 10, -498);
+	vehicle->SetPos(0, 10, 0);
 	
 	vehicle->collision_listeners.add(this);
 	vehicle->SetId(1);
