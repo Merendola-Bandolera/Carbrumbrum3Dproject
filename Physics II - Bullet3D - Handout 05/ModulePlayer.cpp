3df638a773eae89eb6419601a199ec63c2d1ae4e
Onnda <dannoman2004@gmail.com>
2024-01-12 01:47:19 +0100

    CIRCUITO CERRAO

--- Physics II - Bullet3D - Handout 05/ModulePlayer.cpp
+++ Physics II - Bullet3D - Handout 05/ModulePlayer.cpp
@@ -146,7 +146,7 @@ bool ModulePlayer::Start()
 	//car.wheels[7].steering = false;
 
 	vehicle = App->physics->AddVehicle(car);
-	vehicle->SetPos(786, 10, 816);
+	vehicle->SetPos(0, -0.5f, 30);
 	
 	vehicle->collision_listeners.add(this);
 	vehicle->SetId(1);
