764e9178a2a868141dc225c53cef3f1e488ea446
Onnda <dannoman2004@gmail.com>
2024-01-14 01:02:58 +0100

    subido todo a 200 de altura

--- Physics II - Bullet3D - Handout 05/ModulePlayer.cpp
+++ Physics II - Bullet3D - Handout 05/ModulePlayer.cpp
@@ -146,7 +146,7 @@ bool ModulePlayer::Start()
 	//car.wheels[7].steering = false;
 
 	vehicle = App->physics->AddVehicle(car);
-	vehicle->SetPos(0, 65, 70);
+	vehicle->SetPos(200, 206, 643 + 1009);
 	
 	vehicle->collision_listeners.add(this);
 	vehicle->SetId(1);
